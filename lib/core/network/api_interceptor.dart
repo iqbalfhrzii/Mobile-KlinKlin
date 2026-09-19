3de083c9053474582ee16e4b90456ea0bcdc5511
Iqbalfhrzi <iqbalfahrozi41@gmail.com>
2026-09-19 09:39:43 +0800

    clean(network): remove unused foundation import

--- lib/core/network/api_interceptor.dart
+++ lib/core/network/api_interceptor.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../constants/app_constants.dart';
